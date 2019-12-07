da6662852fb66c3dc3e9a7a905d5c6ba68618570
Petr <petr.podolskiy@gmail.com>
2019-12-07 17:43:36 +0300

    Initial commit.

--- js/main.js
+++ js/main.js
@@ -7,23 +7,9 @@ $( document ).ready(function() {
 );
 });
 
-function sendAjaxForm(result_form, ajax_form, url {
-  $.ajax({
-      url:     action_ajax_form.php, 
-      type:     "POST", 
-      dataType: "html", 
-      data: $("#"+ajax_form).serialize(),  
-      success: function(response) { 
-        result = $.parseJSON(response);
-        $('#result_form').html('Имя: '+result.name+'<br>Телефон: '+result.phonenumber);
-    },
-    error: function(response) { 
-          $('#result_form').html('Ошибка. Данные не отправлены.');
-    }
- });
-}
-
-}$(document).ready(function() {  
+ 
+/*
+$(document).ready(function() {  
   $("#validate").keyup(function(){
      
      var email = $("#validate").val();
@@ -49,8 +35,9 @@ function sendAjaxForm(result_form, ajax_form, url {
      });
    
      });
-   
+  */
      function isValidEmailAddress(emailAddress) {
      var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
      return pattern.test(emailAddress);
      }
+     
