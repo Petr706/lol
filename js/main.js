bfc20458637bb1d8f510d683438995382f2c1f51
Petr <petr.podolskiy@gmail.com>
2020-04-17 12:32:41 +0300

    Vrode Tak

--- js/main.js
+++ js/main.js
@@ -8,7 +8,26 @@ $( document ).ready(function() {
 });
 
 $(document).ready(function(){
-	$(".button").click(function(){
+	$(".button__big").click(function(){
 		$(".modal").toggleClass("modal_visible"); return false;
 	});
 });
+
+$(document).ready(function(){
+	$(".modal__close").click(function(){
+		$(".modal").toggleClass("modal_visible"); return false;
+	});
+});
+
+axios.get('https://www.google.ru/search?q=имя')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
